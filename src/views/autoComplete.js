5670b580a7db0db763a32337832801040c9a6e3e
Nalar <thijstel@gmail.com>
2016-01-24 18:05:38 +0100

    Updated to use .on() instead of .keyup

--- src/views/autoComplete.js
+++ src/views/autoComplete.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-	$("#searchTerm").keyup(function(event) {
+	$("#searchTerm").on('keyup',function(event) {
 		$.post(("/matches/?searchTerm="+$("#searchTerm").val()), function(response, status){
 			$("#searchResults").html("<ul>");
 			for(result in response){
-				//$("#searchResults").append(response[result].firstName + " " + response[result].lastName + "<br>");
 				$("#searchResults").append("<li>" + response[result].firstName + " " + response[result].lastName + "</li>");
 			}
 		});
