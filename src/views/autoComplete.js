fab11d60bbd120e2b45f81b1886d09287cb2b2ff
Nalar <thijstel@gmail.com>
2016-01-24 18:28:17 +0100

    Added bandwith throttling with 300 ms

--- src/views/autoComplete.js
+++ src/views/autoComplete.js
@@ -1,11 +1,20 @@
+var requestTime = 0;
+var lastRequestTime = 0;
+
 $(document).ready(function(){
 	$("#searchTerm").on('keyup',function(event) {
-		$.post(("/matches/?searchTerm="+$("#searchTerm").val()), function(response, status){
-			$("#searchResults").html("<ul>");
-			for(result in response){
-				$("#searchResults").append("<li>" + response[result].firstName + " " + response[result].lastName + "</li>");
-			}
-		});
+		requestTime = event.timeStamp;
+		if(requestTime - lastRequestTime > 300 || lastRequestTime === 0){
+			console.log("Updating, difference over 300ms: " + (requestTime-lastRequestTime) + "ms");
+			lastRequestTime = requestTime
+			$.post(("/matches/?searchTerm="+$("#searchTerm").val()), function(response, status){
+				$("#searchResults").html("<ul>");
+				for(result in response){
+					$("#searchResults").append("<li>" + response[result].firstName + " " + response[result].lastName + "</li>");
+				};
+			})
+		} else{
+			console.log("Not updating, difference under 300ms: " + (requestTime-lastRequestTime) + "ms")
+		};;
 	});
 });
-
